4754a707f35cd491e02b01693de3e081caf1efe5
JocimSus <joe.susatiyo@gmail.com>
2026-09-01 07:07:24 +0000

    feat: added whitenoise for static image loading, and added several deps

--- config/settings.py
+++ config/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -68,8 +69,6 @@ TEMPLATES = [
         },
     },
 ]
-
-STATICFILES_DIRS = [BASE_DIR / "static"]
 
 
 # Database
@@ -126,7 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
-STATIC_URL = os.getenv("DJANGO_STATIC_URL", "/static/")
+STATIC_URL = os.getenv("DJANGO_STATIC_URL", "static/")
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+WHITENOISE_USE_FINDERS = True
 
 
 # Email
