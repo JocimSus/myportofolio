ff5c78feb8082c9cec9e559436a09afbc82d2b9f
JocimSus <joe.susatiyo@gmail.com>
2026-09-02 14:37:03 +0700

    feat: moved all project level pages to home page app

--- apps/home/views.py
+++ apps/home/views.py
@@ -3,13 +3,6 @@ from django.shortcuts import render
 
 
 # Create your views here.
-def hello_world(req: HttpRequest) -> HttpResponse:
-    return render(
-        req,
-        "hello_world.html",
-    )
-
-
 def home(req: HttpRequest) -> HttpResponse:
     return render(
         req,
--- config/urls.py
+++ config/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-
-from . import views
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.landing_page, name="landing_page"),
+    path("", include("apps.home.urls")),
 ]
