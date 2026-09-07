de4f2ad504fb895f6255d49f82006f9123ed7e06
JocimSus <joe.susatiyo@gmail.com>
2026-09-07 14:59:35 +0700

    feat: added experience model, and added new experiences page

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = "home"
+
 urlpatterns = [
     path("", views.home, name="home"),
+    path("experience/", views.experience, name="experience"),
 ]
--- apps/home/views.py
+++ apps/home/views.py
@@ -1,10 +1,42 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import Experience
+
 
 # Create your views here.
 def home(req: HttpRequest) -> HttpResponse:
-    return render(
-        req,
-        "home/home.html",
-    )
+    ctx = {
+        "name": "Joachim Susatiyo",
+        "nick": "Joachim",
+        "npm": "2506602694",
+        "study_program": "Ilmu Komputer - S1",
+        "bio": (
+            "a passionate computer science student at Universitas Indonesia."
+            "I love exploring new technologies and applying them to solve real-world problems."
+        ),
+        "interests": [
+            "Web Development",
+            "System Design",
+            "Operating Systems",
+            "Cloud Infrastructure",
+            "Web Development",
+        ],
+        "experience_truncated": [
+            "Winner of RISTEK Hackathon 2026",
+            "Vice Lead of IT Dev OH Fasilkom 2026",
+            "Member of RISTEK Web Development",
+        ],
+    }
+
+    return render(req, "home/home.html", ctx)
+
+
+def experience(req: HttpRequest) -> HttpResponse:
+    ctx = {
+        "name": "Joachim Susatiyo",
+        "nick": "Joachim",
+        "experiences": Experience.objects.all(),
+    }
+
+    return render(req, "home/experience.html", ctx)
