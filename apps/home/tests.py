cc35fd5eb563dbecf2f6e5af9736350f008088c8
JocimSus <joe.susatiyo@gmail.com>
2026-09-07 08:05:57 +0000

    feat: added testing

--- apps/home/tests.py
+++ apps/home/tests.py
@@ -1,1 +1,60 @@
 # Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from django.utils import timezone
+
+from .models import Experience
+
+
+class HomeTest(TestCase):
+    def setUp(self):
+        self.experience = Experience.objects.create(
+            title="Asisten Dosen PBP",
+            description="Membantu mahasiswa memahami pengembangan web.",
+            experience_type="part-time",
+            start_date=timezone.now(),
+        )
+
+    def test_url_is_accessible(self):
+        response = self.client.get(reverse("home:home"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home/home.html")
+        self.assertNotContains(response, self.experience.title)
+        self.assertContains(response, f'href="{reverse("home:experience")}"')
+
+    def test_nonexistent_page_returns_404(self):
+        response = self.client.get("/halaman-yang-tidak-ada/")
+
+        self.assertEqual(response.status_code, 404)
+
+    def test_experience_model(self):
+        self.assertEqual(str(self.experience), "Asisten Dosen PBP")
+        self.assertEqual(self.experience.experience_type, "part-time")
+        self.assertTrue(self.experience.is_ongoing)
+
+    def test_experience_page(self):
+        response = self.client.get(reverse("home:experience"))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, "home/experience.html")
+        self.assertContains(response, self.experience.title)
+        self.assertContains(response, self.experience.description)
+        self.assertContains(response, "Part-Time")
+        self.assertContains(response, "Sedang berlangsung")
+        self.assertContains(response, f'href="{reverse("home:home")}"')
+
+    def test_empty_experience_page(self):
+        Experience.objects.all().delete()
+        response = self.client.get(reverse("home:experience"))
+
+        self.assertContains(response, "Belum ada pengalaman yang ditambahkan.")
+
+    def test_completed_experience(self):
+        self.experience.end_date = timezone.now()
+        self.experience.save()
+        response = self.client.get(reverse("home:experience"))
+
+        self.assertFalse(self.experience.is_ongoing)
+        self.assertContains(response, "Selesai")
+        self.assertNotContains(response, "Sedang berlangsung")
