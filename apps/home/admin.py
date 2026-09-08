e0c9ead60558f0cee3732274912736e3897daf57
JocimSus <joe.susatiyo@gmail.com>
2026-09-08 13:55:52 +0700

    feat: added admin model to add experiences

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,1 +1,29 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Experience
+
+
+@admin.register(Experience)
+class ExperienceAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "start_date", "end_date", "is_ongoing")
+
+    list_filter = ("category", "start_date")
+
+    search_fields = ("title", "description")
+
+    ordering = ("start_date",)
+
+    fieldsets = (
+        (
+            "Basic Information",
+            {
+                "fields": ("title", "category", "thumbnail", "description"),
+            },
+        ),
+        (
+            "Timeline",
+            {
+                "fields": ("start_date", "end_date"),
+            },
+        ),
+    )
