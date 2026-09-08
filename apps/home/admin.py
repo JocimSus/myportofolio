9ee120cf20cb820452653ece6ed84109040738a7
JocimSus <joe.susatiyo@gmail.com>
2026-09-08 15:39:33 +0700

    feat: added admin ui for projects

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Experience
+from .models import Experience, Project
 
 
 @admin.register(Experience)
@@ -24,6 +24,32 @@ class ExperienceAdmin(admin.ModelAdmin):
             "Timeline",
             {
                 "fields": ("start_date", "end_date"),
+            },
+        ),
+    )
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ("title", "category", "project_url")
+
+    list_filter = ("category",)
+
+    search_fields = ("title", "description")
+
+    ordering = ("title",)
+
+    fieldsets = (
+        (
+            "Basic Information",
+            {
+                "fields": ("title", "category", "thumbnail", "description"),
+            },
+        ),
+        (
+            "Details",
+            {
+                "fields": ("technologies", "project_url"),
             },
         ),
     )
