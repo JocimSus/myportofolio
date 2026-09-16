338e7e421f23c01472f6a1345171be94bb04b266
JocimSus <joe.susatiyo@gmail.com>
2026-09-16 05:53:59 +0000

    aa

--- apps/home/context_processors.py
+++ apps/home/context_processors.py
@@ -3,5 +3,5 @@ from django.http import HttpRequest
 from .selectors import get_default_context
 
 
-def default_context_processor(_request: HttpRequest) -> dict:
+def default(_request: HttpRequest) -> dict:
     return get_default_context()
--- config/settings.py
+++ config/settings.py
@@ -77,7 +77,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
-                "apps.home.context_processor.default_context_processor",
+                "apps.home.context_processors.default",
             ],
         },
     },
